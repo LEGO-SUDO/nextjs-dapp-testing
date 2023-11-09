21edf05cab65ec528329dac618ba588e9997e931
NAKUL  SINGH <nakulsingh@NAKULs-MacBook-Air.local>
2023-11-10 01:22:20 +0530

    first commit

---
+++ src/pages/page.js
@@ -0,0 +1,16 @@
+"use client"
+import Image from 'next/image'
+import { useTriaConnector } from "@tria-sdk/connect";
+import Application, {TriaConnectProvider} from '@tria-sdk/authenticate'
+
+
+export default function Home() {
+   const { globalData } = useTriaConnector({ walletUrl: "https://auth-7rin.vercel.app" });
+  return (
+    <div>
+       <TriaConnectProvider/>
+       <Application/>
+      <Home/>
+    </div>
+  )
+}
